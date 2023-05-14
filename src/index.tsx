d3258c2acdc43ff52e24631308bf07d966157883
quedevel <kdh90817@naver.com>
2023-05-14 16:54:55 +0900

    :sparkles: feat: ErrorPage 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ErrorPage from "./ErrorPage";
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -9,6 +10,7 @@ const router = createBrowserRouter([
     {
         path: "/",
         element: <App/>,
+        errorElement: <ErrorPage />,
     },
 ]);
 
