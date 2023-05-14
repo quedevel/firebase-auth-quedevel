d5051a09f73852ebbb8dc6e3f73ffdc130973ad0
quedevel <kdh90817@naver.com>
2023-05-15 00:43:47 +0900

    :sparkles: feat: 페이지별 로그인과 리다이렉트 관리

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,16 +1,34 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import React, { useState, useEffect } from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { auth } from "../firebase.config";
 
 export default function Navigation() {
+    const { user } = useAuth()
+    const [loading, setLoading] = useState(true);
+
+    const handleLogout = async () => {
+        await auth.signOut();
+    };
+
+    useEffect(() => {
+        if (!user) {
+            return;
+        }
+        setLoading(false)
+    }, [user, loading]);
+
     return (
         <>
             <Navbar bg="dark" variant="dark">
                 <Container>
                     <Nav className="me-auto">
-                        <Nav.Link href="/home">Home</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/event">Event</Nav.Link>
-                        <Nav.Link href="/login">Login</Nav.Link>
+                        {!user && <Nav.Link href="/login">Login</Nav.Link>}
+                        {user && <Nav.Link onClick={handleLogout}>Logout</Nav.Link>}
                     </Nav>
                 </Container>
             </Navbar>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,8 @@
  export default function HomePage(){
+
     return(
-        <>
-            <h1>Home</h1>
-        </>
+        <h1 className="homepage-txt">
+            🏠
+        </h1>
     )
  }
