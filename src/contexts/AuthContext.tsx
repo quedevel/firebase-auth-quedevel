937927ea7fc20e1140719d785dbf678a89d7c5dd
quedevel <kdh90817@naver.com>
2023-05-16 10:57:23 +0900

    :zap: update: `GitHub`은 API를 통해서 유저정보를 재조회를 해야한다.

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -10,6 +10,13 @@ type ProviderDataType = {
     providerId: string
     uid: string
 }
+
+type GitHubUserType = {
+    login: string,
+    html_url: string,
+    email: string,
+}
+
 type AuthContextProps = { user: User | null, providerData: ProviderDataType | null}
 type AuthProviderProps = { children: ReactNode }
 
@@ -29,6 +36,15 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
         auth.onAuthStateChanged((user) => {
             setUser(user)
             setProviderData(user?.providerData[0] as ProviderDataType)
+            if (providerData && providerData.providerId === 'github.com') {
+                /* GitHub은 이메일을 주지 않기 때문에 API를 통해서 username을 가져오도록 한다. */
+                getGitHubUserData(providerData.uid).then((res: GitHubUserType) => {
+                    setProviderData({
+                        ...providerData,
+                        displayName: res.login
+                    })
+                })
+            }
             setLoading(false)
         })
     }, [user])
@@ -39,4 +55,18 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
             {!loading && children}
         </AuthContext.Provider>
     )
+}
+
+async function getGitHubUserData(githubIdOrLogin: string) {
+    const header = {headers: {'Accept': 'application/json'}}
+    const apiUrl = `https://api.github.com/user/${githubIdOrLogin}`
+    return fetch(apiUrl, header)
+        .then((res) => {
+            if (!res.ok) {
+                const err = new Error()
+                // TODO res.status 값에 따라 오류 메세지 처리 ( 구찮으니 일단 패쓰 )
+                return Promise.reject(err)
+            }
+            return res.json()
+        })
 }
